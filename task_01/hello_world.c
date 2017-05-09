c256db52346861c43a191cf4e7d30f69262dee19
Jozef Pochyba <jozefpochyba@gmail.com>
2017-05-09 12:31:19 +0200

    Change log level for task 01

--- task_01/hello_world.c
+++ task_01/hello_world.c
@@ -6,12 +6,12 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Task 01 of the Eudyptula Challenge");
 
 static int __init hello_init(void) {
-  printk(KERN_INFO "Hello World!\n");
+  printk(KERN_DEBUG "Hello World!\n");
   return 0;
 }
 
 static void __exit hello_exit(void) {
-  printk(KERN_INFO "Bye World!\n");
+  printk(KERN_DEBUG "Bye World!\n");
 }
 
 module_init(hello_init);
